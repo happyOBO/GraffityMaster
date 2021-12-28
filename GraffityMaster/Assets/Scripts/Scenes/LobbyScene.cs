f8eb0c579b6145a3864eaa0a5745a7090e32c290
happyOBO <obo314@icloud.com>
2021-12-28 12:50:17 +0900

    로비 Scene 추가

--- GraffityMaster/Assets/Scripts/Scenes/LobbyScene.cs
+++ GraffityMaster/Assets/Scripts/Scenes/LobbyScene.cs
@@ -3,24 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoginScene : BaseScene
+public class LobbyScene : BaseScene
 {
 
     protected override void Init()
     {
         List<GameObject> testpool = new List<GameObject>();
         base.Init();
-        sceneType = Define.Scene.Login;
+        sceneType = Define.Scene.Lobby;
 
-        for(int i = 0; i < 5;i++)
-        {
-            testpool.Add(Managers.Resource.Instantiate("UnityChan"));
-        }
-
-        foreach(GameObject obj in testpool)
-        {
-            Managers.Resource.Destroy(obj);
-        }
     }
     private void Update()
     {
@@ -31,6 +22,6 @@ public class LoginScene : BaseScene
     }
     public override void Clear()
     {
-        Debug.Log("Login Scene Clear");
+        Debug.Log("Lobby Scene Clear");
     }
 }
